faab6c83deb61fe7df05daba2143e6c5cf7232ff
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-10-19 17:48:52 +0000

    card layout

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,10 @@
-
+import { useState } from 'react'
+import React from 'react';
 
 function App(){
     return (
         <div>
-
+            <h1 className='text-center text-primary'>Stuff</h1>
         </div>
     )
 }
