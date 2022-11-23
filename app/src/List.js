68c229d055ca988c417f9e05286e8e0fe7f7f2da
Joshtdale <joshua.dale@student.awesomeinc.org>
2022-11-23 17:09:27 +0000

    btn fix

--- app/src/List.js
+++ app/src/List.js
@@ -1,4 +1,4 @@
-
+import trash from './images/trash.svg'
 
 
 function List(props) {
@@ -16,18 +16,22 @@ function List(props) {
 
 
     function complete(item) {
-
-        const completedItem = [...props.storage]
-        const id = completedItem.find(todo => todo.id === item)
-        id.completed = !id.completed
-        props.setStorage(completedItem)
+        setTimeout(() => {
+            const completedItem = [...props.storage]
+            const id = completedItem.find(todo => todo.id === item)
+            id.completed = !id.completed
+            props.setStorage(completedItem)
+        }, "300")
 
     }
 
 
     function handleRemoveItem(id) {
-        const leftOver = props.storage.filter((item) => item.id !== id)
-        props.setStorage(leftOver)
+        setTimeout(() => {
+            const leftOver = props.storage.filter((item) => item.id !== id)
+            props.setStorage(leftOver)
+        }, "300")
+
     };
 
 
@@ -42,29 +46,36 @@ function List(props) {
                 <div className="overflow-auto">
 
                     {todoItems.map((item) => {
-                        let border = 'card border-success'
+                        let border = 'card mb-2 border-success'
                         let textDecoration = ''
-                        let greenbtn = 'h-50 btn rounded-circle border-success mt-2 bg-success'
+                        // let greenbtn = 'h-50 btn rounded-circle border-success mt-2 bg-success'
 
                         if (item.completed === true) {
-                            border = 'card border-danger opacity-75 mb-2'
+                                border = 'card border-danger opacity-75 mb-2'
                             textDecoration = "text-decoration-line-through"
-                            greenbtn = 'h-50 btn rounded-circle border-success mt-2 disabled bg-success opacity-10'
-                        } else {
-                            border = 'card border-success mb-2'
+                            //     greenbtn = 'h-50 btn rounded-circle border-success mt-2 disabled bg-success opacity-10'
                         }
+                        //  else {
+                        //     border = 'card border-success mb-2'
+                        // }
 
                         return (
                             <div className={border} key={item.id}>
                                 <div className="row text-center">
-                                    <div className="col m-2">
-                                        <button onClick={() => complete(item.id)} className={greenbtn}></button>
+                                    <div className="col m-2 d-flex justify-content-center align-items-center">
+                                        {/* <button onClick={() => complete(item.id)} className={greenbtn}></button> */}
+                                        <div class="form-check">
+                                            <input onClick={() => complete(item.id)} class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+                                            {/* <label class="form-check-label" for="flexCheckDefault">
+                                                    Default checkbox
+                                                </label> */}
+                                        </div>
                                     </div>
                                     <div className="col m-2">
                                         <p className={textDecoration} >{item.text}</p>
                                     </div>
-                                    <div className="col m-2">
-                                        <button onClick={() => handleRemoveItem(item.id)} className="h-50 btn rounded-circle border-danger bg-danger mt-2"></button>
+                                    <div className="col m-2 d-flex justify-content-center align-items-center">
+                                        <i onClick={() => handleRemoveItem(item.id)}><img src={trash} className="btn"></img></i>
                                     </div>
                                 </div>
                             </div>
